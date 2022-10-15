e1e966070c3d380cb120b583d414085255e156cb
StoicaMircea <theunknownhack@protonmail.com>
2022-10-15 22:52:32 +0300

    Working on Mini Calculator App using Java AWT library

--- Mini calculator AWT/src/Calculator.java
+++ Mini calculator AWT/src/Calculator.java
@@ -11,11 +11,55 @@ public  class Calculator extends Frame {
         frame.setSize(500,500);
         frame.setBackground(Color.GRAY);
 
-        Button calculeaza = new Button("Calculeaza");
-        calculeaza.setPreferredSize(new Dimension(100, 25));
-        frame.add(calculeaza);
+        LayoutManager gridBagLayout = new GridBagLayout();
+        frame.setLayout(gridBagLayout);
 
-        GridBagLayout gridBagLayout = new GridBagLayout();
+
+        //Adaugarea primelo numere
+        TextField primulNumar = new TextField("Add first number:");
+        frame.add(primulNumar);
+
+        TextField numarSecund = new TextField("Add second number:");
+        frame.add(numarSecund);
+
+        //Selectarea operatiei matematice
+        Choice operatie = new Choice();
+        operatie.add("+");
+        operatie.add("-");
+        operatie.add("*");
+        operatie.add("/");
+        frame.add(operatie);
+
+        //Afisarea rezultatului
+
+
+        GridBagConstraints gridBagConstraints;
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        frame.add(primulNumar, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        frame.add(numarSecund, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        frame.add(operatie, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        //frame.add(, gridBagConstraints);
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        //frame.add(, gridBagConstraints);
+
+
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
