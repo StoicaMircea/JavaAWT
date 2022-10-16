8f745c03fabc28cf7006ae076213cfbaf70fd06b
StoicaMircea <theunknownhack@protonmail.com>
2022-10-16 21:50:35 +0300

    Performed changes

--- Mini calculator AWT/src/Calculator.java
+++ Mini calculator AWT/src/Calculator.java
@@ -9,57 +9,68 @@ public  class Calculator extends Frame {
         frame.setTitle("Mini Calculator App");
         frame.setVisible(true);
         frame.setSize(500,500);
-        frame.setBackground(Color.GRAY);
 
         LayoutManager gridBagLayout = new GridBagLayout();
         frame.setLayout(gridBagLayout);
 
+        Label firstNumber = new Label("Add a number");
+        frame.add(firstNumber);
 
-        //Adaugarea primelo numere
-        TextField primulNumar = new TextField("Add first number:");
-        frame.add(primulNumar);
+        TextField numar1 = new TextField();
+        frame.add(numar1);
 
-        TextField numarSecund = new TextField("Add second number:");
-        frame.add(numarSecund);
+        Label secondNumber = new Label("Add another number");
+        frame.add(secondNumber);
 
-        //Selectarea operatiei matematice
+        TextField numar2 = new TextField();
+        frame.add(numar2);
+
         Choice operatie = new Choice();
-        operatie.add("+");
-        operatie.add("-");
-        operatie.add("*");
-        operatie.add("/");
+        operatie.add("+"); operatie.add("-");
+        operatie.add("*"); operatie.add("/");
         frame.add(operatie);
 
-        //Afisarea rezultatului
+        Button calcul = new Button("Calculate");
+        frame.add(calcul);
 
+        Label result = new Label("Result is");
+        frame.add(result);
 
-        GridBagConstraints gridBagConstraints;
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        frame.add(primulNumar, gridBagConstraints);
+        GridBagConstraints afisare;
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        frame.add(numarSecund, gridBagConstraints);
+        afisare = new GridBagConstraints();
+        afisare.gridx = 0;
+        afisare.gridy = 0;
+        frame.add(firstNumber, afisare);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        frame.add(operatie, gridBagConstraints);
+        afisare = new GridBagConstraints();
+        afisare.gridx = 0;
+        afisare.gridy = 1;
+        afisare.gridwidth = 50;
+        afisare.gridheight = 50;
+        frame.add(numar1, afisare);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        //frame.add(, gridBagConstraints);
+        afisare = new GridBagConstraints();
+        afisare.gridx = 1;
+        afisare.gridy = 0;
+        frame.add(secondNumber, afisare);
 
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        //frame.add(, gridBagConstraints);
+        afisare = new GridBagConstraints();
+        afisare.gridx = 1;
+        afisare.gridy = 1;
+        afisare.gridwidth = 50;
+        afisare.gridheight = 50;
+        frame.add(numar2, afisare);
 
+        afisare = new GridBagConstraints();
+        afisare.gridx = 2;
+        afisare.gridy = 1;
+        frame.add(operatie, afisare);
 
+        afisare = new GridBagConstraints();
+        afisare.gridx = 3;
+        afisare.gridy = 0;
+        frame.add(result, afisare);
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
