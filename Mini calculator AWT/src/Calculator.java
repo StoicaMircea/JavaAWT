937fce2069e331677b9c49575272775da25cc24e
StoicaMircea <theunknownhack@protonmail.com>
2022-10-18 01:27:45 +0300

    Almost finished assignment

--- Mini calculator AWT/src/Calculator.java
+++ Mini calculator AWT/src/Calculator.java
@@ -1,76 +1,95 @@
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
-public  class Calculator extends Frame {
-
+public abstract class Calculator extends Frame implements ActionListener {
     public static void main(String[] args) {
-        Frame frame= new Frame();
-        frame.setTitle("Mini Calculator App");
-        frame.setVisible(true);
-        frame.setSize(500,500);
+        Frame frame = new Frame("Frame container");
 
-        LayoutManager gridBagLayout = new GridBagLayout();
-        frame.setLayout(gridBagLayout);
+        Label label = new Label("First number: ");
+        Label anotherLabel = new Label("Second number: ");
+        Label selectOperation = new Label("Select operation: ");
+        Label result = new Label("Result is: ");
 
-        Label firstNumber = new Label("Add a number");
-        frame.add(firstNumber);
-
-        TextField numar1 = new TextField();
-        frame.add(numar1);
-
-        Label secondNumber = new Label("Add another number");
-        frame.add(secondNumber);
-
-        TextField numar2 = new TextField();
-        frame.add(numar2);
-
-        Choice operatie = new Choice();
-        operatie.add("+"); operatie.add("-");
-        operatie.add("*"); operatie.add("/");
-        frame.add(operatie);
-
-        Button calcul = new Button("Calculate");
-        frame.add(calcul);
-
-        Label result = new Label("Result is");
+        frame.add(label);
+        frame.add(anotherLabel);
+        frame.add(selectOperation);
         frame.add(result);
 
-        GridBagConstraints afisare;
+        TextField firstNumber = new TextField(5);
+        TextField secondNumber = new TextField(5);
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 0;
-        afisare.gridy = 0;
-        frame.add(firstNumber, afisare);
+        frame.add(firstNumber);
+        frame.add(secondNumber);
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 0;
-        afisare.gridy = 1;
-        afisare.gridwidth = 50;
-        afisare.gridheight = 50;
-        frame.add(numar1, afisare);
+        Choice operation = new Choice();
+        operation.add("+"); operation.add("-"); operation.add("*"); operation.add("/");
+        frame.add(operation);
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 1;
-        afisare.gridy = 0;
-        frame.add(secondNumber, afisare);
+        Button calculate = new Button("Calculate");
+        frame.add(calculate);
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 1;
-        afisare.gridy = 1;
-        afisare.gridwidth = 50;
-        afisare.gridheight = 50;
-        frame.add(numar2, afisare);
+        firstNumber.addTextListener(new TextListener() {
+            @Override
+            public void textValueChanged(TextEvent e) {
+                TextField inputText = (TextField) e.getSource();
+                int numar1 = Integer.parseInt(String.valueOf(inputText));
+            }
+        });
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 2;
-        afisare.gridy = 1;
-        frame.add(operatie, afisare);
+        secondNumber.addTextListener(new TextListener() {
+            @Override
+            public void textValueChanged(TextEvent e) {
+                TextField inputText = (TextField) e.getSource();
+                int numar2 = Integer.parseInt(String.valueOf(inputText));
+            }
+        });
 
-        afisare = new GridBagConstraints();
-        afisare.gridx = 3;
-        afisare.gridy = 0;
-        frame.add(result, afisare);
+
+
+        //Setam layout-ul frame-ului
+
+        GridBagLayout layout = new GridBagLayout();
+        frame.setLayout(layout);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        frame.add(label, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        frame.add(firstNumber, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        frame.add(anotherLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        frame.add(secondNumber, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        frame.add(selectOperation, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        frame.add(operation, gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        frame.add(calculate, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridheight = 2;
+        frame.add(result, gbc);
+
+        frame.setSize(500, 500);
+        frame.setTitle("Mini Calculator App");
+        frame.setBackground(Color.LIGHT_GRAY);
+        frame.setResizable(false);
+        frame.setVisible(true);
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
